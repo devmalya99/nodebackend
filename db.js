1bbaa6a73f77f678b8be126f36c41a29c8b52551
Debmalya <debmalyamazumdar99@gmail.com>
2025-08-13 16:06:55 +0530

    V8

--- db.js
+++ db.js
@@ -2,21 +2,16 @@
 import mongoose from "mongoose";
 
 export const connectDB = async (retries = 5, delay = 5000) => {
-  let mongo_url;
-  if (process.env.ENV_MODE == "development") {
-    mongo_url = process.env.MONGODB_URI_DEV;
-  } else {
-    mongo_url = process.env.MONGODB_URI_PROD;
-  }
-  if (!mongo_url) {
+ 
+  if (!process.env.MONGODB_URI_DEV) {
     console.error("MONGODB_URI is not defined in environment variables");
     process.exit(1);
   }
 
   try {
-    await mongoose.connect(mongo_url);
+    await mongoose.connect(process.env.MONGODB_URI_DEV);
     console.log("✅ MongoDB connected successfully");
-    console.log(`Server is running on ${process.env.ENV_MODE} mode`)
+    console.log(`Server is running on ${process.env.MONGODB_URI_DEV} mode`);
   } catch (error) {
     console.error("❌ MongoDB connection error:", error.message);
     if (retries > 0) {
