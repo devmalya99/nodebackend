e9c8a1de9917b40a3a54850f9cfacd68bdad74be
shubhamsharmayo <shubham232108@gmail.com>
2025-08-13 14:45:07 +0530

    dynamic db selection

--- db.js
+++ db.js
@@ -1,23 +1,34 @@
 // db/db.js
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 export const connectDB = async (retries = 5, delay = 5000) => {
-    if (!process.env.MONGODB_URI) {
-        console.error('MONGODB_URI is not defined in environment variables');
-        process.exit(1);
-    }
+  let mongo_url;
+  if (process.env.ENV_MODE == "development") {
+    mongo_url = process.env.MONGODB_URI_DEV;
+  } else {
+    mongo_url = process.env.MONGODB_URI_PROD;
+  }
+  if (!mongo_url) {
+    console.error("MONGODB_URI is not defined in environment variables");
+    process.exit(1);
+  }
 
-    try {
-        await mongoose.connect(process.env.MONGODB_URI);
-        console.log('✅ MongoDB connected successfully');
-    } catch (error) {
-        console.error('❌ MongoDB connection error:', error.message);
-        if (retries > 0) {
-            console.log(`🔁 Retrying connection in ${delay / 1000}s... (${retries} attempts remaining)`);
-            setTimeout(() => connectDB(retries - 1, delay), delay);
-        } else {
-            console.error('💥 Failed to connect to MongoDB after multiple attempts');
-            process.exit(1);
-        }
+  try {
+    await mongoose.connect(mongo_url);
+    console.log("✅ MongoDB connected successfully");
+    console.log(`Server is running on ${process.env.ENV_MODE} mode`)
+  } catch (error) {
+    console.error("❌ MongoDB connection error:", error.message);
+    if (retries > 0) {
+      console.log(
+        `🔁 Retrying connection in ${
+          delay / 1000
+        }s... (${retries} attempts remaining)`
+      );
+      setTimeout(() => connectDB(retries - 1, delay), delay);
+    } else {
+      console.error("💥 Failed to connect to MongoDB after multiple attempts");
+      process.exit(1);
     }
+  }
 };
